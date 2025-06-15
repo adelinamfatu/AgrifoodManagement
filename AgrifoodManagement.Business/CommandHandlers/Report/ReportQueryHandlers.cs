0a7351a47e0ed59acf8966bf7ec46fe164f1111d
Adelina Fatu <adelinamfatu@gmail.com>
2025-06-15 12:52:14 +0300

    Added page titles

--- AgrifoodManagement.Business/CommandHandlers/Report/ReportQueryHandlers.cs
+++ AgrifoodManagement.Business/CommandHandlers/Report/ReportQueryHandlers.cs
@@ -4,12 +4,6 @@ using AgrifoodManagement.Util.Models;
 using AgrifoodManagement.Util.ValueObjects;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace AgrifoodManagement.Business.CommandHandlers.Report
 {
